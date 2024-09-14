53d588a74ce99b80d5145f93afe974fa32993a3f
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2024-09-14 14:00:10 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Models\Phone;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/one_to_one', function () {
+//     $users=User::find(1)->phone;
+//     $phones=Phone::find(1);
+//     echo $phones->user;
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
